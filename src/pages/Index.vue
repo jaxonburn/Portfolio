87ccc674b462f684110cc135d1b6ef38b771573a
jaxonburningham <jaxon@ionrev.com>
2021-06-07 16:51:02 -0600

    Initial SetUp

---
+++ src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center" style="overflow: hidden;height: 80vh;">
+    <div>
+    <lottie-animation
+      ref="loadingAnim"
+      :auto-play="true"
+      :loop="true"
+      :animationData="require('assets/LottieAnims/blob.json')"
+      :style="$q.screen.lt.sm ? 'height: 300px;width: 300px;' : 'height: 800px; width: 800px;'"
+    >
+    </lottie-animation>
+    </div>
+  </q-page>
+</template>
+
+<script>
+  import LottieAnimation from 'lottie-web-vue';
+export default {
+  name: 'PageIndex',
+  components: {
+    LottieAnimation
+  }
+}
+</script>
